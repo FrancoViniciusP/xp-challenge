654826dbf1c72b55cfb73b034b27e4406a377700
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-07-16 16:46:58 -0300

    feat: create a simple carteira router

---
+++ src/pages/Carteira.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Carteira() {
+  return (
+    <main>
+      <p>Carteira</p>
+    </main>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Carteira from '../pages/Carteira';
 import Login from '../pages/Login';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/carteira" element={<Carteira />} />
     </Routes>
   );
 }
