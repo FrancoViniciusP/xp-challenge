9edc147e4cf5d0c9e39f382c6bba4030a04a779b
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-07-16 13:51:03 -0300

    feat: config redux, store and reducer
    n

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>learn react</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
