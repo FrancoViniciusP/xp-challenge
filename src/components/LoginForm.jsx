823efa941aff44066edb1703fcac4a539bb4ae4a
Vinicius Pacheco <vpacheco0427@gmail.com>
2022-07-16 14:54:53 -0300

    feat: add form and logo image on login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
 export default function LoginForm() {
-  return <h1>Login</h1>;
+  return (
+    <form type="submit">
+      <label htmlFor="email">
+        Email
+        <input id="email" type="text" />
+      </label>
+      <label htmlFor="password">
+        Senha
+        <input id="password" type="text" />
+      </label>
+
+      <button type="submit" aria-label="entrar" disabled> ENTRAR</button>
+
+      <text>
+        Esqueceu a senha?
+        <strong> Clique Aqui</strong>
+      </text>
+
+    </form>
+
+  );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,5 +2,11 @@ import React from 'react';
 import LoginForm from '../components/LoginForm';
 
 export default function Login() {
-  return <LoginForm />;
+  return (
+    <main>
+      <img src="xp_icon_white.png" alt="logo xp investimentos" />
+      <LoginForm />
+
+    </main>
+  );
 }
